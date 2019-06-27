cd72682a3d90fad1a5e6f8a0d7dba6fbf03ef869
naliniganesan <51950030+naliniganesan@users.noreply.github.com>
2019-06-27 11:12:52 +0530

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -2,8 +2,8 @@ n=int(input())
 if n>1:
    for i in range(2,3):
       if(n%i==0):
-         print("yes")
-      else:
          print("no")
+      else:
+         print("yes")
 else:
    print("no")
