d86ae10db7cd51a28aab3e3ccfd1685440c2bcc7
naliniganesan <51950030+naliniganesan@users.noreply.github.com>
2019-07-08 13:48:15 +0530

    Update ex26.py

--- ex26.py
+++ ex26.py
@@ -1,5 +1,5 @@
-u = int(input())
-v = list(map(int,input().split()))
-v.sort()
-for i in range(u):
-  print(v[i],end=" ")
+u1 = int(input())
+v1 = list(map(int,input().split()))
+v1.sort()
+for i in range(u1):
+  print(v1[i],end=" ")
